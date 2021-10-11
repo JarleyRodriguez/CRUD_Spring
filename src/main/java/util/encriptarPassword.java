66e5779f9b83c2027869840d347f283b0a8ac917
jarley <rodriguez14jarley@gmail.com>
2021-10-11 13:57:17 -0500

    se agregaron header y footer en login y index

--- src/main/java/util/encriptarPassword.java
+++ src/main/java/util/encriptarPassword.java
@@ -3,15 +3,16 @@ package util;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public class encriptarPassword {
+
     public static void main(String[] args) {
         //clase para encriptar contraseñas es de spring
-        
-        var password ="54321";
+
+        var password = "54321";
         System.out.println("password :" + password);
         System.out.println("password Encrytado: " + encriptarPassword(password));
     }
-    
-    public static String encriptarPassword(String password){
+
+    public static String encriptarPassword(String password) {
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
         return encoder.encode(password);
     }
